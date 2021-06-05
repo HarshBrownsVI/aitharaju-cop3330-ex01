0ac0bfd08ba33287d138f2e003b306bd27bd2521
HarshBrownsVI <harshaaitharaju15@gmail.com>
2021-06-05 16:57:13 -0400

    Exercise 1 Solution

---
+++ Ex1.java
@@ -0,0 +1,20 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Sriharsha Aitharaju
+ */
+
+
+import java.util.*;
+public class Ex1{
+
+    public static void main(String[]args)
+    {
+        Scanner kb = new Scanner(System.in);
+        String s = "";
+        System.out.println("What is your name?");
+        s = kb.next();
+        System.out.println("Hello, " + s + " nice to meet you!");
+    }
+
+
+}
